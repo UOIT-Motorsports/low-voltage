046aa37253091bfd051f65eb0f646b1e7eb365ca
Mack Zahn <mackenzie.zahn@uoit.net>
2018-10-19 12:08:03 -0400

    timestamps

--- Telemetry Code/Master_Code_without_Accelerometer/Master_Code_without_Accelerometer.ino
+++ Telemetry Code/Master_Code_without_Accelerometer/Master_Code_without_Accelerometer.ino
@@ -46,6 +46,11 @@ int angle = 0;
 
 int x = 0;
 int button = 0;
+int startTime = 0;
+unsigned long timeNow;
+int milliseconds = 0;
+int seconds = 0;
+int minutes = 0;
 void setup() {
    Serial.begin(2000000);
  Serial.println ("Starting...");
@@ -104,174 +109,165 @@ Serial.print("Initializing SD card...");
 
   // create a new file 
 
-for (uint8_t i = 0; i < 100; i++) {
-filename[6] = i/10 + '0';
-filename[7] = i%10 + '0';
-if (! SD.exists(filename)) {
-// only open a new file if it doesn't exist
-logfile = SD.open(filename, FILE_WRITE);
- logfile.println ("        Time   |   Steering Angle | Braking | Throttle | FL Disp | FR Disp | BL Disp | BR Disp | Temp 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | Accerometer | Gforce");
-break; // leave the loop!
-}
-}
-Serial.println(filename);
- pinMode(13, OUTPUT);
- pinMode(2, INPUT);
-   digitalWrite(13,LOW);
-Serial.println("Push Button to Start");
-while (digitalRead(2) == LOW) {}
-digitalWrite(13,HIGH);
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = i/10 + '0';
+    filename[7] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE);
+      logfile.println ("        Time   |   Steering Angle | Braking | Throttle | FL Disp | FR Disp | BL Disp | BR Disp | Temp 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | Accerometer | Gforce");
+      break; // leave the loop!
+    }
+  }
+  Serial.println(filename);
+   pinMode(13, OUTPUT);
+   pinMode(2, INPUT);
+     digitalWrite(13,LOW);
+  Serial.println("Push Button to Start");
+  while (digitalRead(2) == LOW) {}
+  startTime = millis();
+  digitalWrite(13,HIGH);
 }
 
 void loop() {
- 
-
-DataLogger();
-SteeringAngle();
-Displacement();
-TempSensors();
-
-
-
-
-
+  
+  DataLogger();
+  SteeringAngle();
+  Displacement();
+  TempSensors();
+  while(((millis()- startTime) %100) !=0){}
+  
 }
 
 void DataLogger(){
 
 
-DateTime now = rtc.now();  
-
- Time = ("(" +String(now.year()) + "/" + String(now.month()) + "/" + String( now.day()) + ") " +  String (now.hour())  + ":" + String(now.minute())  +":" + String (now.second()));
+  timeNow = millis() - startTime;
+  milliseconds = timeNow%1000;
+  seconds = (timeNow/1000)%60;
+  minutes = (timeNow/60000);
+  Time = String(minutes)+ ":" + String(seconds) + ":" + String(milliseconds);
+  // ("(" +String(now.year()) + "/" + String(now.month()) + "/" + String( now.day()) + ") " +  String (now.hour())  + ":" + String(now.minute())  +":" + String (now.second()));
   
-// make a string for assembling the data to log:
-  datastring = (Time+ "   " + datastring);
-
+  // make a string for assembling the data to log:
+  datastring = (Time+ ", " + datastring);
+  
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-logfile = SD.open(filename, FILE_WRITE);
-
-
-  // if the file is available, write to it:
- if (logfile) {
+  logfile = SD.open(filename, FILE_WRITE);
   
+  
+  // if the file is available, write to it:
+  if (logfile) {
     logfile.println(datastring);
-   logfile.close();
+    logfile.close();
     // print to the serial port too:
-     Serial.println(datastring);
-
- }
+    Serial.println(datastring);
+  }
   
   // if the file isn't open, pop up an error:
   else {
     Serial.println("error opening file");
- }
-datastring = String ("");
-  
+  }
+  datastring = String ("");
+
 }
 
 
 void SteeringAngle(){
-   
- sensorValue = analogRead(A1);
- 
-int wheel = map (sensorValue,0,1023,0,345);
-datastring = (datastring + String(wheel) + "  ");
-
-
-int sensorValue1 = analogRead(A2);
- 
-int brake = map (sensorValue1,0,1023,0,345);
-datastring = (datastring + String(brake) + "  ");
-
-
+  sensorValue = analogRead(A1);
   
+  int wheel = map (sensorValue,0,1023,0,345);
+  datastring = (datastring + String(wheel) + ",  ");
+  
+  
+  int sensorValue1 = analogRead(A2);
+  
+  int brake = map (sensorValue1,0,1023,0,345);
+  datastring = (datastring + String(brake) + ",  ");
 }
 
 void Displacement(){
-   
-int SFL = analogRead(A3);
- 
-int FL = map (SFL,0,1023,200,0);
-datastring = (datastring + String(FL) + "  ");
-
-int SFR = analogRead(A4);
- 
-int FR = map (SFR,0,1023,200,0);
-datastring = (datastring + String(FR) + "  ");
-
-/*
-int SBL = analogRead(A5);
-int BL = map (SBL,0,1023,200,0);
-datastring = (datastring + String(BL) + "  ");
-
-
-int SBR = analogRead(A6);
- 
-int BR = map (SBR,0,1023,0,200);
-datastring = (datastring + String(BR) + "  ");
+  int SFL = analogRead(A3);
+  
+  int FL = map (SFL,0,1023,200,0);
+  datastring = (datastring + String(FL) + ", ");
+  
+  int SFR = analogRead(A4);
+  
+  int FR = map (SFR,0,1023,200,0);
+  datastring = (datastring + String(FR) + ", ");
+  
+  /*
+  int SBL = analogRead(A5);
+  int BL = map (SBL,0,1023,200,0);
+  datastring = (datastring + String(BL) + "  ");
+  
+  
+  int SBR = analogRead(A6);
+  
+  int BR = map (SBR,0,1023,0,200);
+  datastring = (datastring + String(BR) + "  ");
   */
 }
 
 
 void TempSensors(){
- 
-
-if(therm1.read()){
-  double Temp1 = therm1.object();
-  datastring = (datastring + String(Temp1) + "  ");
-}
-if(therm2.read()){
-   double Temp2 = therm2.object();
-  datastring = (datastring + String(Temp2) + "  ");
-}
-
-if(therm3.read()){
-  double Temp3 = therm3.object();
-  datastring = (datastring + String(Temp3) + "  ");
-}
-if(therm4.read()){
-  double Temp4 = therm4.object();
-  datastring = (datastring + String(Temp4) + "  ");
-}
-
-if(therm5.read()){
-  double Temp5 = therm5.object();
-  datastring = (datastring + String(Temp5) + "  ");
-}
-
-if(therm6.read()){
-  double Temp6 = therm6.object();
-  datastring = (datastring + String(Temp6) + "  ");
-}
-
-if(therm7.read()){
-  double Temp7 = therm7.object();
-  datastring = (datastring + String(Temp7) + "  ");
-}
-
-if(therm8.read()){
-  double Temp8 = therm8.object();
-  datastring = (datastring + String(Temp8) + "  ");
-}
-
-if(therm9.read()){
-  double Temp9 = therm9.object();
-  datastring = (datastring + String(Temp9) + "  ");
-}
-
-if(therm10.read()){
-  double Temp10 = therm10.object();
-  datastring = (datastring + String(Temp10) + "  ");
-}
-
-if(therm11.read()){
-  double Temp11 = therm11.object();
-  datastring = (datastring + String(Temp11) + "  ");
-}
-
-if(therm12.read()){
-  double Temp12 = therm12.object();
-  datastring = (datastring + String(Temp12) + "  ");
-}
-}
+  if(therm1.read()){
+    double Temp1 = therm1.object();
+    datastring = (datastring + String(Temp1) + ", ");
+  }
+  if(therm2.read()){
+     double Temp2 = therm2.object();
+    datastring = (datastring + String(Temp2) + ", ");
+  }
+  
+  if(therm3.read()){
+    double Temp3 = therm3.object();
+    datastring = (datastring + String(Temp3) + ", ");
+  }
+  if(therm4.read()){
+    double Temp4 = therm4.object();
+    datastring = (datastring + String(Temp4) + ", ");
+  }
+  
+  if(therm5.read()){
+    double Temp5 = therm5.object();
+    datastring = (datastring + String(Temp5) + "  ");
+  }
+  
+  if(therm6.read()){
+    double Temp6 = therm6.object();
+    datastring = (datastring + String(Temp6) + ", ");
+  }
+  
+  if(therm7.read()){
+    double Temp7 = therm7.object();
+    datastring = (datastring + String(Temp7) + ", ");
+  }
+  
+  if(therm8.read()){
+    double Temp8 = therm8.object();
+    datastring = (datastring + String(Temp8) + ", ");
+  }
+  
+  if(therm9.read()){
+    double Temp9 = therm9.object();
+    datastring = (datastring + String(Temp9) + ", ");
+  }
+  
+  if(therm10.read()){
+    double Temp10 = therm10.object();
+    datastring = (datastring + String(Temp10) + ", ");
+  }
+  
+  if(therm11.read()){
+    double Temp11 = therm11.object();
+    datastring = (datastring + String(Temp11) + ", ");
+  }
+  
+  if(therm12.read()){
+    double Temp12 = therm12.object();
+    datastring = (datastring + String(Temp12) + ", ");
+  }
+  }
