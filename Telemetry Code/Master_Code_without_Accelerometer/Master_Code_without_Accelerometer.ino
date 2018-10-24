a9d64c1ac755f903958da685b471df3adf06664c
liamHowatt <liamjmh0@gmail.com>
2018-10-23 22:47:52 -0400

    Update Telemetry Code/Master_Code_without_Accelerometer/Master_Code_without_Accelerometer.ino
    Added half the code necessary to make the accelerometer work. Mainloop stuff still required

--- Telemetry Code/Master_Code_without_Accelerometer/Master_Code_without_Accelerometer.ino
+++ Telemetry Code/Master_Code_without_Accelerometer/Master_Code_without_Accelerometer.ino
@@ -2,12 +2,8 @@
 #include <SparkFunMLX90614.h> // SparkFunMLX90614 Arduino library
 
 
-#include <I2Cdev.h>
-
-
-
-//#include "I2Cdev.h"
-//#include "MPU6050.h"
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
 #include <SPI.h>
 #include <SD.h>
 #include "RTClib.h"
@@ -38,6 +34,31 @@ IRTherm therm9;
 IRTherm therm10;
 IRTherm therm11;
 IRTherm therm12;
+
+//accelerometer
+MPU6050 mpu(0x69);
+#define OUTPUT_READABLE_WORLDACCEL
+#define INTERRUPT_PIN 18
+bool blinkState = false;
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
 
 //Analog Sensors
 
@@ -95,6 +116,35 @@ void setup() {
 
 //Steering Angle
 int sensorValue;
+
+//accelerometer
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+  if(!mpu.testConnection()){
+    Serial.println("accelerometer error. Stopping");
+    while(1);
+  }
+  devStatus = mpu.dmpInitialize();
+  mpu.setXGyroOffset(24);
+  mpu.setYGyroOffset(25);
+  mpu.setZGyroOffset(85);
+  mpu.setXAccelOffset(-2999);
+  mpu.setYAccelOffset(-4445);
+  mpu.setZAccelOffset(1199);
+  if(devStatus != 0){
+    Serial.print("accelerometer error ");
+    Serial.print(devStatus);
+    Serial.println(". Stopping");
+    while(1);
+  }
+  mpu.setDMPEnabled(true);
+  digitalPinToInterrupt(INTERRUPT_PIN)
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+  mpuIntStatus = mpu.getIntStatus();
+  dmpReady = true;
+  packetSize = mpu.dmpGetFIFOPacketSize();
 
 //DataLogger
 Serial.print("Initializing SD card...");
@@ -271,3 +321,7 @@ void TempSensors(){
     datastring = (datastring + String(Temp12) + ", ");
   }
   }
+
+void Accelerometer(){
+  
+}
